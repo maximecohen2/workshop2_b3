7aa3542ae3b27496e1cab78925e854100f945fa4
Revy <doretquentin@gmail.com>
2018-04-10 11:15:20 +0200

    Feat(login, fonts, urls.py)

--- base/urls.py
+++ base/urls.py
@@ -20,5 +20,6 @@ from django.views.generic import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    url(r'^$', TemplateView.as_view(template_name='index.html'))
+    url(r'^$', TemplateView.as_view(template_name='index.html')),
+    url(r'^login$', TemplateView.as_view(template_name='login.html'))
 ]
