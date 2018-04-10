f2d6acabb30e544f47f1ed55e8fa1c6f86246261
Revy <doretquentin@gmail.com>
2018-04-10 11:22:33 +0200

    Feat(login, fonts, urls.py)

--- base/urls.py
+++ base/urls.py
@@ -16,9 +16,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from django.conf.urls import url
-from base.views import home
+from django.views.generic import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    url(r'^$', home, name='home')
+    url(r'^$', home, name='home'),
+    url(r'^login$', TemplateView.as_view(template_name='login.html'))
+
 ]
