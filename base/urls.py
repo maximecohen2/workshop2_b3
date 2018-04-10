4f6165bc3a91914c326deaad83f784f86df5dad1
maximecohen2 <maxime.cohen-pro@outlook.fr>
2018-04-10 11:36:19 +0200

    modif urls

--- base/urls.py
+++ base/urls.py
@@ -17,6 +17,7 @@ from django.contrib import admin
 from django.urls import path
 from django.conf.urls import url
 from django.views.generic import TemplateView
+from base.views import home
 
 urlpatterns = [
     path('admin/', admin.site.urls),
