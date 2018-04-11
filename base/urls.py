50a06e6a1e0a581ab1b67c98dd7257dd2274552f
Revy <doretquentin@gmail.com>
2018-04-11 10:23:11 +0200

    Ajout groupe

--- base/urls.py
+++ base/urls.py
@@ -26,6 +26,7 @@ urlpatterns = [
     url(r'^logout$', LogoutUser.as_view(), name='logout'),
     url(r'^$', login_required(TemplateView.as_view(template_name='index.html'))),
     url(r'^profil', login_required(TemplateView.as_view(template_name='profil.html'))),
+    url(r'^groupe', login_required(TemplateView.as_view(template_name='groupe.html'))),
 
     url(r'^$', login_required(TemplateView.as_view(template_name='index.html'))),
     ]
