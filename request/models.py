ebe18fc1e59f19e4c62fd0aff8c4c558dbee229a
maximecohen2 <maxime.cohen-pro@outlook.fr>
2018-04-12 16:18:35 +0200

    ajout model request

--- request/models.py
+++ request/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Request(models.Model):
+    title = models.CharField(verbose_name="Titre")
+    QUESTION = 0
+    NEW_TEAM = 1
+    DATE = 2
+    TYPE_CHOICE = (
+        (QUESTION, "Question"),
+        (NEW_TEAM, "Nouveau groupe"),
+        (DATE, "Rendez-vous")
+    )
+    type = models.IntegerField(choices=TYPE_CHOICE, default=QUESTION)
+
+
+    class Meta:
+        verbose_name = "Demande"
